3d076d494b2b7d6a8f8e834ab671da4fd2c37544
Gaurang Khera <khera.gaurang@gmail.com>
2023-03-12 22:24:13 +0530

    a

--- dump_analyse.py
+++ dump_analyse.py
@@ -1,7 +1,7 @@
 import dump
 import requests
 from bs4 import BeautifulSoup as bs
-# import validators
+import validators
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
@@ -17,7 +17,6 @@ titles = []
 prices = []
 sizes = []
 
-
 class Sneaker(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
@@ -25,14 +24,12 @@ class Sneaker(db.Model):
     img = db.Column(db.String)
     url = db.Column(db.String)
     sizes = db.relationship('Size', backref='snkr')
-
-
+    
 class Size(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     size = db.Column(db.String)
     sneaker = db.Column(db.Integer, db.ForeignKey('sneaker.id'))
-
-
+    
 app.app_context().push()
 db.create_all()
 
@@ -46,19 +43,16 @@ headers = {
 #         vnv = bs(req.content, 'lxml')
 #         print(vnv.find('h1').text)
 #     except Exception as e:
-#         print(i, e)
+#        print(i, e)
 
 # for i in list(dict.fromkeys(dump.superkicks)):
 #     try:
 #         req = requests.get(i, headers=headers)
 #         sk = bs(req.content, 'lxml')
-#         sizes = sk.find_all('label', class_='OutOfStock') + \
-#             sk.find_all('label', class_='InStock')
-#         img = sk.find(
-#             'div', class_='product__media media media--transparent gradient global-media-settings').find('img').get('src')
+#         sizes = sk.find_all('label', class_='OutOfStock') + sk.find_all('label', class_='InStock')
+#         img = sk.find('div', class_='product__media media media--transparent gradient global-media-settings').find('img').get('src')
 #         size = [x.text.strip() for x in sizes]
-#         new_snkr = Sneaker(name=sk.find('title').text.strip().replace('\n', ''), url=i, img=img, price=sk.find(
-#             'span', class_='price-item price-item--regular').text.strip().replace('\n', ''))
+#         new_snkr = Sneaker(name=sk.find('title').text.strip().replace('\n', ''),url=i,img=img, price=sk.find('span', class_='price-item price-item--regular').text.strip().replace('\n', ''))
 #         new_size = ''
 #         for k in sizes:
 #             new_size = Size(size=k.text.strip(), sneaker=new_snkr.id)
@@ -67,36 +61,34 @@ headers = {
 #         db.session.commit()
 #     except Exception as e:
 #         print(i, e)
-
+        
 # for i in list(dict.fromkeys(dump.dawntown)):
 #     try:
 #         req = requests.get(i, headers=headers)
 #         sk = bs(req.content, 'lxml')
-#         img = sk.find(
-#             'div', class_='product__media media media--transparent media__height').find('img').get('src')
-#         new_snkr = Sneaker(name=sk.find('title').text.strip().replace('\n', ''), url=i, price=sk.find(
-#             'span', class_='price-item price-item--regular').text.strip().replace('\n', ''), img=img)
+#         img = sk.find('div', class_='product__media media media--transparent media__height').find('img').get('src')
+#         new_snkr = Sneaker(name=sk.find('title').text.strip().replace('\n', ''),url=i, price=sk.find('span', class_='price-item price-item--regular').text.strip().replace('\n', ''), img=img)
 #         new_size = ''
-#         sizes = size = sk.find('variant-radios').find_all('label')
+#         sizes = size=sk.find('variant-radios').find_all('label')
 #         for j in sizes:
-#             new_size = Size(size=j.text.strip(), sneaker=new_snkr.id)
+#             new_size  = Size(size=j.text.strip(), sneaker=new_snkr.id)
 #             new_snkr.sizes.append(new_size)
 #             db.session.add(new_size)
 #         db.session.add(new_snkr)
 #         db.session.commit()
 #     except Exception as e:
 #         print(i, e)
-
-# for i in list(dict.fromkeys(dump.mainstreet)):
-#     k = 'https://marketplace.mainstreet.co.in/' + i
+        
+# for i in list(dict.fromkeys(dump.crepdog)):
 #     try:
-#         req = requests.get(k, headers=headers)
+#         req = requests.get(i, headers=headers)
 #         sk = bs(req.content, 'lxml')
-#         new_snkr = Sneaker(name=sk.find('h1', class_='product-single__title').text.strip().replace('\n', ''),url=k, price=sk.find('span', class_='product-single__price').text.strip().replace('\n', ''))
+#         img = sk.find('img', class_='Image--fadeIn lazyautosizes Image--lazyLoaded')
+#         new_snkr = Sneaker(name=sk.find('h1', class_='ProductMeta__Title Heading u-h2').text.strip().replace('\n', ''),url=i, price=sk.find('span', class_='money').text.strip().replace('\n', ''), img=img)
 #         new_size = ''
-#         sizes = sk.find('select', class_='single-option-selector').find_all('option')
+#         sizes = sk.find('div', class_='Popover__ValueList').find_all('button')
 #         for j in sizes:
-#             new_size = Size(size=j.text.strip(), sneaker=new_snkr.id)
+#             new_size  = Size(size=j.text.strip(), sneaker=new_snkr.id)
 #             new_snkr.sizes.append(new_size)
 #             db.session.add(new_size)
 #         db.session.add(new_snkr)
@@ -125,15 +117,14 @@ headers = {
 #     try:
 #         req = requests.get(i, headers=headers)
 #         cd = bs(req.content, 'lxml')
-#         snkr = Sneaker.query.filter_by(name=cd.find(
-#             'h1', class_='ProductMeta__Title Heading u-h2').text.strip().replace('\n', '')).first()
-#         img = cd.find('div', class_='Product__SlideItem Product__SlideItem--image Carousel__Cell is-selected').find(
-#             'img').get('data-src').replace('{width}', '1400')
+#         snkr = Sneaker.query.filter_by(name=cd.find('h1', class_='ProductMeta__Title Heading u-h2').text.strip().replace('\n', '')).first()
+#         img = cd.find('div', class_='Product__SlideItem Product__SlideItem--image Carousel__Cell is-selected').find('img').get('data-src').replace('{width}', '1400')
 #         snkr.img = img
 #         db.session.add(snkr)
 #         db.session.commit()
 #     except Exception as e:
 #         print(e)
+
 
 
 class SearchForm(FlaskForm):
